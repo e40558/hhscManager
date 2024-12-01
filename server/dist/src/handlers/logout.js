52faf661c3d9fceb12b9941006a260eed78f96ae
Andre McCaskill <mccaskillhld@yahoo.com>
2024-12-01 15:32:52 -0600

    consumermodule

--- server/dist/src/handlers/logout.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.logout = void 0;
-function logout(request, response, nex) {
-    response.clearCookie("SESSIONID");
-    response.clearCookie("XSRF-TOKEN");
-    response.status(200);
-}
-exports.logout = logout;
